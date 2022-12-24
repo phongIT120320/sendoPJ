2773c5e81da8a92cc4c7c7d947a108cd2c3ce335
phong <phamhongphong120300@gmail.com>
2022-12-24 08:41:58 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,250 @@
+// Đối tượng `Validator`
+function Validator(options) {
+    function getParent(element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    }
+
+    var selectorRules = {};
+
+    // Hàm thực hiện validate
+    function validate(inputElement, rule) {
+        var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
+        var errorMessage;
+
+        // Lấy ra các rules của selector
+        var rules = selectorRules[rule.selector];
+
+        // Lặp qua từng rule & kiểm tra
+        // Nếu có lỗi thì dừng việc kiểm
+        for (var i = 0; i < rules.length; ++i) {
+            switch (inputElement.type) {
+                case 'radio':
+                case 'checkbox':
+                    errorMessage = rules[i](
+                        formElement.querySelector(rule.selector + ':checked')
+                    );
+                    break;
+                default:
+                    errorMessage = rules[i](inputElement.value);
+            }
+            if (errorMessage) break;
+        }
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            getParent(inputElement, options.formGroupSelector).classList.add('invalid');
+        } else {
+            errorElement.innerText = '';
+            getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+        }
+
+        return !errorMessage;
+    }
+
+    // Lấy element của form cần validate
+    var formElement = document.querySelector(options.form);
+    if (formElement) {
+        // Khi submit form
+        formElement.onsubmit = function (e) {
+            e.preventDefault();
+
+            var isFormValid = true;
+
+            // Lặp qua từng rules và validate
+            options.rules.forEach(function (rule) {
+                var inputElement = formElement.querySelector(rule.selector);
+                var isValid = validate(inputElement, rule);
+                if (!isValid) {
+                    isFormValid = false;
+                }
+            });
+
+            if (isFormValid) {
+                // Trường hợp submit với javascript
+                if (typeof options.onSubmit === 'function') {
+                    var enableInputs = formElement.querySelectorAll('[name]');
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
+
+                        switch (input.type) {
+                            case 'radio':
+                                values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value;
+                                break;
+                            case 'checkbox':
+                                if (!input.matches(':checked')) {
+                                    values[input.name] = '';
+                                    return values;
+                                }
+                                if (!Array.isArray(values[input.name])) {
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
+                                break;
+                            default:
+                                values[input.name] = input.value;
+                        }
+
+                        return values;
+                    }, {});
+                    options.onSubmit(formValues);
+                }
+                // Trường hợp submit với hành vi mặc định
+                else {
+                    formElement.submit();
+                }
+            }
+        }
+
+        // Lặp qua mỗi rule và xử lý (lắng nghe sự kiện blur, input, ...)
+        options.rules.forEach(function (rule) {
+
+            // Lưu lại các rules cho mỗi input
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            } else {
+                selectorRules[rule.selector] = [rule.test];
+            }
+
+            var inputElements = formElement.querySelectorAll(rule.selector);
+
+            Array.from(inputElements).forEach(function (inputElement) {
+                // Xử lý trường hợp blur khỏi input
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);
+                }
+
+                // Xử lý mỗi khi người dùng nhập vào input
+                inputElement.oninput = function () {
+                    var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
+                    errorElement.innerText = '';
+                    getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+                }
+            });
+        });
+    }
+
+}
+
+
+
+// Định nghĩa rules
+// Nguyên tắc của các rules:
+// 1. Khi có lỗi => Trả ra message lỗi
+// 2. Khi hợp lệ => Không trả ra cái gì cả (undefined)
+Validator.isRequired = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value ? undefined : message || 'Vui lòng nhập trường này'
+        }
+    };
+}
+
+Validator.isEmail = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : message || 'Trường này phải là email';
+        }
+    };
+}
+
+Validator.minLength = function (selector, min, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} kí tự`;
+        }
+    };
+}
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value === getConfirmValue() ? undefined : message || 'Giá trị nhập vào không chính xác';
+        }
+    }
+}
+
+
+
+
+// Chức năng tìm kiếm sản phẩm
+var eleInput = document.querySelector('.form-input');
+eleInput.oninput = (e) => {
+    var textInput = e.target.value.trim().toLowerCase();
+    var listProduct = document.querySelectorAll('.card-product');
+    console.log(listProduct)
+
+    listProduct.forEach((item) => {
+        console.log(item.innerText.toLowerCase())
+        if (item.innerText.toLowerCase().includes(textInput)) {
+            item.classList.remove('hide');
+        } else {
+            item.classList.add('hide');
+        }
+    })
+}
+
+// Xử lí render ra danh sách sản phẩm
+fetch('http://fakestoreapi.com/products')
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        var eleProduct = document.querySelector('.item-product');
+        data.forEach(product => {
+            let itemProduct = document.createElement('div');
+            itemProduct.classList.add('p-1');
+            itemProduct.classList.add('card-product');
+            itemProduct.innerHTML = `
+        <div class="card text-white">
+            <div class="card-img-top d-flex">
+               <img class="" src="${product.image}" alt=""> 
+            </div>
+            <div class="card-body p-2">
+                <div class="sale d-flex">
+                    <img src="./img/ele.png" alt="" class="img-fluid mr-1">
+                    <img src="./img/ele2.png" alt="" class="img-fluid">
+                </div>
+                <div class="desc mt-2">
+                    <img src="./img/hop.png" alt="" class="img-fluid">
+                    <p>${product.description}</p>
+                </div>
+                <span class="price">${product.price}đ</span>
+                <span class="price-mask">150,000đ</span>
+                <div class="route">
+                    Đã bán 1
+                </div>
+            </div>
+        </div>
+        `
+            eleProduct.appendChild(itemProduct);
+        })
+    })
+
+
+// Xử lí Time Tab
+let timeEle = document.querySelectorAll('.time__item');
+timeEle.forEach((item) => {
+    item.onclick = () => {
+        let activeEle = document.querySelector('.time__item.time__tab-active')
+        activeEle.classList.remove('time__tab-active')
+        item.classList.add('time__tab-active')
+    }
+})
+
+
+
+
+
+
